79ec93fb88d91a95219ec7bb6821e8b627ccb16e
Max Czapanskiy <maxczapanskiy@gmail.com>
2014-01-08 23:01:48 -0800

    initial commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by Max Czapanskiy on 1/8/14.
+//  Copyright (c) 2014 Max Czapanskiy. All rights reserved.
+//
+// https://developer.apple.com/library/mac/qa/qa1613/_index.html
+
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
